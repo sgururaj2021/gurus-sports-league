ecff735de96c8f2e18d008f0164a199fb8abd0ac
sgururaj2021 <gururaj1931@gmail.com>
2021-09-23 15:15:47 +0530

    Wagtail Project gslsite first commit

---
+++ gslsite/gslsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&5wg603wm4=ly!2v=*1v178#!7xr)lhwx%05%7t!=hf6qx-odp'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
